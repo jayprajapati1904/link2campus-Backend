6d5abddf8204f486b122b8722edfd7324a765331
Jay <jay1904prajapati@gmail.com>
2025-03-31 19:55:09 +0530

    Remove authentication middleware from the admin users route for direct access.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/auth.js");
+// const authMiddleware = require("../middleware/auth.js");
 const adminController = require("../Controller/adminController.js");
 
-router.get("/users", authMiddleware.verifyToken, adminController.getAllUsers);
+router.get("/users", adminController.getAllUsers);
 
 router.post("/create", adminController.createAdmin);
 
