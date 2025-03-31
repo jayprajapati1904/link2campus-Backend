cf667e2a04fe76280948f4c56263e3635d520cae
Jay <jay1904prajapati@gmail.com>
2025-03-31 19:59:00 +0530

    Add authentication middleware to the admin users route for enhanced security.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const authMiddleware = require("../middleware/auth.js");
+const authMiddleware = require("../middleware/auth.js");
 const adminController = require("../Controller/adminController.js");
 
-router.get("/users", adminController.getAllUsers);
+router.get("/users", authMiddleware.verifyToken, adminController.getAllUsers);
 
 router.post("/create", adminController.createAdmin);
 
